7bbbd032849a5fa2c144dc34340fa5915c5055a3
Rezal <rladlfdnd369@gmail.com>
2023-06-02 15:22:06 +0900

    FEAT(page added)

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,16 +1,24 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function List() {
     
+    const food = ['tomato', 'pasta', 'coconut'];
+
     return (
         <div>
             <h4 className="title">상품목록</h4>
-            <div className="food">
-                <h4>상품1 $40</h4>
-            </div>
-            <div className="food">
-                <h4>상품2 $20</h4>
-            </div>
+
+                { food.map((element, index) => {
+
+                    return (
+                        <div className="food" key={index}>
+                            <img src={`${food[index]}.png`} className="food-img" alt={`${food[index]}!`}
+                                 width={200} />
+                            <h4>{element} <br/><br/>- Price : $40</h4>
+                        </div>
+                    );
+                }) }
 
             <Link href="/" style={{color: "blue", textDecorationLine: "underline"}}>홈으로</Link>
         </div>
