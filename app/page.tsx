f9b0554f8febd6ee53471e92c0c65bc56127635a
Rezal <rladlfdnd369@gmail.com>
2023-06-02 14:25:48 +0900

    SET

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
+  
+  let name = 'kim';
+
   return (
-    <main>
-      <div>
-        <h1>Home</h1>
+
+    <div>
+      <div className='navbar'>
+        <Link href='/'>홈</Link>
+        <Link href='/list'>상품목록</Link>
       </div>
-    </main>
-    
+      <h4 className='title'>apple fresh</h4>
+      <p className='title-sub'>by dev {name}</p>
+
+    </div>
   )
 }
